fb2f4128ebbbbb9a9aa415091fbb8ed1a13ac8f8
Armin Knaul <armin@Armins-MBP.fritz.box>
2022-02-07 17:09:54 +0100

    added chakraUI basics and am now pushing for device change

--- entry_system_website/pages/index.js
+++ entry_system_website/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { useAuth } from '../components/Firebase/Context/authUserContext'
 import { useEffect } from 'react'
 import { AspectRatio, Box, Center, Container, Flex, Image, useColorMode ,Button, IconButton, HStack} from '@chakra-ui/react';
-import { PlusSquareIcon } from '@chakra-ui/icons';
+import { LockIcon, PlusSquareIcon, SunIcon } from '@chakra-ui/icons';
 
 
 
@@ -14,24 +14,22 @@ export default function Home() {
   let { user, Firebase_signOut , signIn} = useAuth();
 
   return (
-    <Container>
-      <Center>
-        <Flex>
-          <Image src="svg/Logo.jpg" objectFit="contain"/>
+    <Box>
+        <Flex justify="space-between" padding="20px">
+          <Image src="./svg/Logo.jpg" objectFit="contain"/>
           <HStack>
-            <Button leftIcon={<PlusSquareIcon/>}  colorScheme='teal' variant="solid">
-              Sign Up
+            <Button leftIcon={<LockIcon />} colorScheme='teal' variant='ghost'>
+              Sign In
             </Button>
-
-
+            <Button leftIcon={<PlusSquareIcon />} colorScheme='teal' variant='ghost'>
+              Create Account
+            </Button>
+            <Button size='sm' onClick={toggleColorMode}>
+              <SunIcon/>
+            </Button>
           </HStack>
         </Flex>
 
-        <section>
-          <div className={styles.Lock}></div>
-          <div className={styles.BottomLeftPeople}></div>
-        </section>
-      </Center>
-    </Container>
+    </Box>
   )
 }
