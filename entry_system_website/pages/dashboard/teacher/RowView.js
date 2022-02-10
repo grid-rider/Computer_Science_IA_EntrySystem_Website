e6c4a7d34003c81ff7d970ef97f3f47ee1098fd1
grid-rider <62141711+grid-rider@users.noreply.github.com>
2022-02-10 03:11:50 +0100

    implemented simple row directory

--- entry_system_website/pages/dashboard/teacher/RowView.js
+++ entry_system_website/pages/dashboard/teacher/RowView.js
@@ -1,19 +1,34 @@
 
-import { Flex, Heading, Text, VStack } from '@chakra-ui/react';
+import { Flex, Heading, List, ListItem, Text, VStack } from '@chakra-ui/react';
 import Layout from '../../../components/React/Layout/Layout';
 import menuItems from '../../../components/Helpers/TeacherMenuItems';
+import { useEffect, useState } from 'react';
+import {useAuth} from '../../../components/Firebase/Context/authUserContext';
 
 let PageMenuItems = menuItems(false,true,false);
 
 export default function RowView() {
 
-    
+    let [dispalyStudentList, setDisplayStudentLIst] = useState([]);
+    let { studentList } = useAuth();
+
+    useEffect(() => {
+        let tempArray = studentList.map((element) => {
+            let tempDocumentData = element.data()
+            return(<ListItem key={element.id}><Text>{tempDocumentData.email}</Text></ListItem>)
+        })
+        setDisplayStudentLIst(tempArray)
+    }, [studentList])
+
     
     return(
-        <Flex flexDir="column">
+        <Flex flexDir="column" justifyContent="space-evenly">
             <Heading> Row View</Heading>
             <VStack>
                 <Text>Student Directory</Text>
+                <List>
+                    {dispalyStudentList}
+                </List>
             </VStack>
         </Flex>
     )
