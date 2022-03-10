50430e78d97eb240fd5a1416f665a65896254cab
Armin Knaul <armin@Armins-MBP.fritz.box>
2022-03-10 19:55:57 +0100

    start student view. QR code scanning showing some problems. Will continue working...

--- entry_system_website/pages/dashboard/student/StudentView.js
+++ entry_system_website/pages/dashboard/student/StudentView.js
@@ -2,13 +2,40 @@
 import { Flex, Heading } from '@chakra-ui/react';
 import Layout from '../../../components/React/Layout/Layout';
 import menuItems from '../../../components/Helpers/StudentMenuItems';
+import { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
 
 let PageMenuItems = menuItems(false,true);
 
 export default function StudentView() {
+
+    const [data, setData] = useState('No result');
+
+    function handleError(){
+
+    }
+
+    function handleScan(d){
+        if (d) {
+            setData(d);
+        }
+    }
+
     return(
         <Flex>
             <Heading> Analytics </Heading>
+            <QrReader
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+            onResult={handleError}
+            onLoad={"mirrorVideo"}
+            showViewFinder= {true}
+            style={{ width: '100px', height:"100px" }}
+            />
+            <div>
+                {data}
+            </div>
         </Flex>
     )
 }
