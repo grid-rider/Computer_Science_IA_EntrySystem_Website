9dad4bddaf19a2d8087c539871fd2fef8b08d799
Armin Knaul <armin@Armins-MacBook-Pro.local>
2022-03-18 11:40:10 +0100

    changed zIndex for overflow fix attempt

--- entry_system_website/pages/dashboard/student/StudentView.js
+++ entry_system_website/pages/dashboard/student/StudentView.js
@@ -102,8 +102,8 @@ export default function StudentView() {
     return(
         <>
             <Flex flexDir="column" justifyContent="space-evenly" alignItems="center">
-                <Flex flexDir="column" justifyContent="center" alignItems="center" sx={{overflow: "hidden"}} position="relative" borderRadius="1000rem" border="solid 2px white" height={{base:"19em",md:"md"}} width={{base:"19em",md:"md"}}>
-                    <Box id="reader" display={scanOn? "inline":"none"} width={{base:"33em",md:"xl"}} height={{base:"33em",md:"xl"}}  ></Box>
+                <Flex flexDir="column" zIndex="0" justifyContent="center" alignItems="center" sx={{overflow: "hidden"}} position="relative" borderRadius="1000rem" border="solid 2px white" height={{base:"19em",md:"md"}} width={{base:"19em",md:"md"}}>
+                    <Box id="reader" zIndex="-1" display={scanOn? "inline":"none"} width={{base:"33em",md:"xl"}} height={{base:"33em",md:"xl"}}></Box>
                     {scanOn?        
                         <Flex position="absolute" border="white 3px solid" p="7em" top="2em">
                             <Text>Scan Me</Text>
