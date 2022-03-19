e09199e2b4db4498b484d02ea941749ecc99ba86
Armin Knaul <armin@Armins-MBP.fritz.box>
2022-03-19 22:42:00 +0100

    added account page and started making index responsive

---
+++ entry_system_website/pages/account.js
@@ -0,0 +1,14 @@
+import { Flex, Heading } from '@chakra-ui/react';
+import UserProfile from '../components/React/Profile';
+
+export default function Account(){
+    if(!user){
+        return(<Text>Acess Denied: Please Log In</Text>)
+    }
+
+    return(
+        <Flex flexDir="coloumn" alignItems="center">
+            <UserProfile/>
+        </Flex>
+    )
+}
--- entry_system_website/pages/index.js
+++ entry_system_website/pages/index.js
@@ -1,67 +1,95 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import { useAuth } from '../components/Firebase/Context/authUserContext'
-import { useEffect, useState } from 'react'
-import { AspectRatio, Box, Center, Container, Flex, Image, useColorMode ,Button, IconButton, HStack, useDisclosure, Modal, ModalOverlay, ModalContent} from '@chakra-ui/react';
-import { LockIcon, PlusSquareIcon, SunIcon , UnlockIcon} from '@chakra-ui/icons';
-import UserProfile from '../components/React/Profile';
+  import Head from 'next/head'
+  import styles from '../styles/Home.module.css'
+  import Link from 'next/link'
+  import { useAuth } from '../components/Firebase/Context/authUserContext'
+  import { useEffect, useState } from 'react'
+  import { AspectRatio, Box, Menu, MenuItem, Center, Container, Flex, Image, useColorMode ,Button, IconButton, HStack, useDisclosure, Modal, ModalOverlay, ModalContent, MenuButton, MenuList} from '@chakra-ui/react';
+  import { LockIcon, PlusSquareIcon, SunIcon , UnlockIcon} from '@chakra-ui/icons';
+  import UserProfile from '../components/React/Profile';
+  import {FaUserCircle} from "react-icons/fa";
+  import { HamburgerIcon } from '@chakra-ui/icons'
 
-const initialRoleLandingPages = {
-  "teacher": "RowView",
-  "student": "StudentView"
-}
-
-
-export default function Home() {
-
-  const { colorMode, toggleColorMode } = useColorMode()
+  const initialRoleLandingPages = {
+    "teacher": "RowView",
+    "student": "StudentView"
+  }
 
 
-  let { user, Firebase_signOut, userData} = useAuth();
-  let [dashboardButtonLoading, setDashboardButtonLoading] = useState(true)
-  let [dashboardSelector, setDashboardSelector] = useState("")
-  
-  useEffect(() => {
-    if(userData){
-      setDashboardSelector("dashboard/" + userData.role + "/" + initialRoleLandingPages[userData.role]);
-      setDashboardButtonLoading(false);
-    } else {
-      setDashboardButtonLoading(true);
-    }
-  }, [userData]);
+  export default function Home() {
 
-  return (
-    <Box>
-        <Flex justify="space-between" padding="20px">
-          <Image src="./svg/Logo.jpg" objectFit="contain"/>
-          <HStack>
-            {user ? 
-              <>
-                <Button leftIcon={<UnlockIcon />} onClick={Firebase_signOut} colorScheme='teal' variant='ghost'>
-                  Sign Out
+    const { colorMode, toggleColorMode } = useColorMode()
+
+
+    let { user, Firebase_signOut, userData} = useAuth();
+    let [dashboardButtonLoading, setDashboardButtonLoading] = useState(true)
+    let [dashboardSelector, setDashboardSelector] = useState("")
+    
+    useEffect(() => {
+      if(userData){
+        setDashboardSelector("dashboard/" + userData.role + "/" + initialRoleLandingPages[userData.role]);
+        setDashboardButtonLoading(false);
+      } else {
+        setDashboardButtonLoading(true);
+      }
+    }, [userData]);
+
+    return (
+      <Box>
+          <Flex flexDir="column">
+            <Flex flexDir="row" justifyContent="space-between" alignItems="center" padding="1em">
+              <Image src="./svg/Logo.jpg" objectFit="contain"/>
+              <HStack display={{base:"none",xl:"inline"}}>
+                {user ? 
+                  <>
+                    <Button leftIcon={<UnlockIcon />} onClick={Firebase_signOut} colorScheme='teal' variant='ghost'>
+                      Sign Out
+                    </Button>
+                    <UserProfile/>
+                    <Button colorScheme='blue' variant='solid' isLoading={dashboardButtonLoading}>
+                      <Link href={dashboardSelector}>Dashboard</Link>
+                    </Button> 
+                  </>
+                  :
+                  <>
+                    <Button leftIcon={<LockIcon />} colorScheme='teal' variant='ghost'>
+                      <Link href="/SignInPage">Sign In</Link>
+                    </Button>
+                    <Button leftIcon={<PlusSquareIcon />} colorScheme='teal' variant='ghost'>
+                      <Link href="/SignUpPage">Create Account</Link>
+                    </Button>
+                  </>
+                }
+              </HStack>
+              <HStack>
+                <Menu display={{base:"inline",xl:"none"}}>
+                  <MenuButton as={IconButton} variant="unstyled" icon={<HamburgerIcon w="1.5em" h="1.5em"/>}>Menu
+                  </MenuButton>
+                  <MenuList>
+                    {user ? 
+                      <>
+                        <MenuItem icon={<FaUserCircle/>}>Profile</MenuItem>
+                        <MenuItem>Sign Out</MenuItem>
+
+                      </>
+                      :
+                      <>
+                        <MenuItem>Sign In</MenuItem>
+                        <MenuItem>Create Account</MenuItem>
+                      </>
+                    }
+                  </MenuList>
+                </Menu>
+                <Button onClick={toggleColorMode}>
+                    <SunIcon/>
                 </Button>
-                <UserProfile/>
-                <Button colorScheme='blue' variant='solid' isLoading={dashboardButtonLoading}>
-                  <Link href={dashboardSelector}>Dashboard</Link>
-                </Button> 
-              </>
-              :
-              <>
-                <Button leftIcon={<LockIcon />} colorScheme='teal' variant='ghost'>
-                  <Link href="/SignInPage">Sign In</Link>
-                </Button>
-                <Button leftIcon={<PlusSquareIcon />} colorScheme='teal' variant='ghost'>
-                  <Link href="/SignUpPage">Create Account</Link>
-                </Button>
-              </>
-            }
+              </HStack>
 
-            <Button onClick={toggleColorMode}>
-              <SunIcon/>
-            </Button>
-          </HStack>
-        </Flex>
-    </Box>
-  )
-}
+            </Flex>
+
+            
+
+
+          </Flex>
+      </Box>
+    )
+  }
