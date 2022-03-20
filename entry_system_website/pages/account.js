1ffaa42e0694424b44388d9f018daf122adb0b71
Armin Knaul <armin@Armins-MBP.fritz.box>
2022-03-20 10:17:17 +0100

    added useAuth to account.js

--- entry_system_website/pages/account.js
+++ entry_system_website/pages/account.js
@@ -1,7 +1,11 @@
 import { Flex, Heading, Text} from '@chakra-ui/react';
+import { useAuth } from '../components/Firebase/Context/authUserContext';
 import UserProfile from '../components/React/Profile';
 
 export default function Account(){
+
+    let { user } = useAuth();
+    
     if(!user){
         return(<Text>Acess Denied: Please Log In</Text>)
     }
