393bed60d42a88112b72efa4abd2765f552cfd9d
grid-rider <62141711+grid-rider@users.noreply.github.com>
2022-03-20 17:41:30 +0100

    added entry and exit date to student view

--- entry_system_website/pages/dashboard/student/StudentView.js
+++ entry_system_website/pages/dashboard/student/StudentView.js
@@ -1,135 +1,166 @@
 
-    import { Box, Button, Flex, Heading, HStack, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, Text, useDisclosure, VStack } from '@chakra-ui/react';
-    import Layout from '../../../components/React/Layout/Layout';
-    import menuItems from '../../../components/Helpers/StudentMenuItems';
-    import { useEffect, useState } from 'react';
-    import {Html5QrcodeScanner, Html5Qrcode} from "html5-qrcode";
-    import { useAuth } from '../../../components/Firebase/Context/authUserContext';
-    import UserProfile from '../../../components/React/Profile';
+import { Box, Button, Flex, Heading, HStack, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, Stat, StatHelpText, StatLabel, StatNumber, Text, useDisclosure, VStack } from '@chakra-ui/react';
+import Layout from '../../../components/React/Layout/Layout';
+import menuItems from '../../../components/Helpers/StudentMenuItems';
+import { useEffect, useState } from 'react';
+import {Html5QrcodeScanner, Html5Qrcode} from "html5-qrcode";
+import { useAuth } from '../../../components/Firebase/Context/authUserContext';
+import UserProfile from '../../../components/React/Profile';
+import NavBar from '../../../components/React/View/NavBar';
 
 
-    const qrScannerConfig = {
-        fps: 15,
+const qrScannerConfig = {
+    fps: 15,
+}
+
+
+function getParsedDate(date){
+    return(date.getDay() + "." + date.getMonth() +"." + date.getFullYear());
+}
+
+function getParsedTime(date){
+    return(date.getMinutes() + ":" + date.getHours());
+}
+
+let scanner;    
+
+export default function StudentView() {
+
+
+
+
+    const { isOpen, onOpen, onClose} = useDisclosure();
+    let [ scanOn, setScanOn] = useState(false);
+    let [ entryType, setEntryType] = useState("loading...");
+    let [ entryObject, setEntryObject] = useState(null);
+    let [ name, setName] = useState("loading...");
+
+
+    //useAuth object destructuring 
+    let { setBuildingTransfer,acessStations,updateStudentEntryStatus, user, userData} = useAuth();
+
+
+    useEffect(() => {
+        
+        if(userData){
+            let entry_date =  new Date(userData.last_entry.seconds*1000 + userData.last_entry.nanoseconds/100000);
+            let exit_date = new Date(userData.last_exit.seconds*1000 + userData.last_exit.nanoseconds/100000);
+            setEntryObject({
+                entry_date_string: getParsedDate(entry_date),
+                entry_time_string: getParsedTime(entry_date),
+                exit_date_string: getParsedDate(exit_date),
+                exit_time_string: getParsedTime(exit_date),
+            });
+            if(userData.entry_status){
+                setEntryType("Signed In")
+            }else{
+                setEntryType("Signed Out")
+            }
+            setName(userData.first_name + " " + userData.last_name)
+        }
+    }, [userData])
+
+
+
+    function startQrCode () {
+
+        scanner = new Html5Qrcode("reader");    
+        Html5Qrcode.getCameras().then((devices) => {
+            scanner.start({ facingMode: "environment" }, qrScannerConfig, onScanSucess, onScanFailure).catch((error) => {
+                console.log("failed to start scanner")
+                throw error 
+            })
+        }).catch((error) => {
+            console.log("error occured with QR code scanner : " + error)
+        })
+
     }
 
-    let scanner;    
 
-    export default function StudentView() {
-
-
-        const { isOpen, onOpen, onClose} = useDisclosure();
-        let [ scanOn, setScanOn] = useState(false);
-        let [ entryType, setEntryType] = useState("loading...");
-        let [ name, setName] = useState("loading...");
-
-
-        //useAuth object destructuring 
-        let { setBuildingTransfer,acessStations,updateStudentEntryStatus, user, userData} = useAuth();
-
-
-        useEffect(() => {
-            
-            if(userData){
-                if(userData.entry_status){
-                    setEntryType("Signed In")
-                }else{
-                    setEntryType("Signed Out")
-
-                }
-                setName(userData.first_name + " " + userData.last_name)
-            }
-        }, [userData])
-
-
-
-        function startQrCode () {
-
-            scanner = new Html5Qrcode("reader");    
-            Html5Qrcode.getCameras().then((devices) => {
-                scanner.start({ facingMode: "environment" }, qrScannerConfig, onScanSucess, onScanFailure).catch((error) => {
-                    console.log("failed to start scanner")
-                    throw error 
-                })
-            }).catch((error) => {
-                console.log("error occured with QR code scanner : " + error)
-            })
-
-        }
-
-
-        //event listener functions for HTMLQRCode scanner
-        function onScanSucess(decodedText, decodedResult){  
-            scanner.pause(true);
-            acessStations.forEach((doc) => {
-                if(decodedText == doc.id) {
-                    console.log("got here")
-                    updateStudentEntryStatus(!userData.entry_status,user.uid).then(() => {
-                        setBuildingTransfer((userData.entry_status? "entry":"exit"),decodedText).then(() => {
-                            onOpen()
-                            toggleScan()
-                            return 0;
-                        }).catch((error) => {
-                            console.log(error);
-                        })
+    //event listener functions for HTMLQRCode scanner
+    function onScanSucess(decodedText, decodedResult){  
+        scanner.pause(true);
+        acessStations.forEach((doc) => {
+            if(decodedText == doc.id) {
+                console.log("got here")
+                updateStudentEntryStatus(!userData.entry_status,user.uid).then(() => {
+                    setBuildingTransfer((userData.entry_status? "entry":"exit"),decodedText).then(() => {
+                        onOpen()
+                        toggleScan()
+                        return 0;
                     }).catch((error) => {
-                        console.log(error)
+                        console.log(error);
                     })
-                }
-            });
-            scanner.pause(false);
-        }
-
-        function onScanFailure(error) {
-            console.log(error)
-        }   
-
-        function toggleScan(){
-            if(!scanOn){
-                setScanOn(value => !value)
-                startQrCode()
-            }else{
-                scanner.stop().then(() => {
-                    setScanOn(value => !value);
+                }).catch((error) => {
+                    console.log(error)
                 })
             }
+        });
+        scanner.pause(false);
+    }
+
+    function onScanFailure(error) {
+        console.log(error)
+    }   
+
+    function toggleScan(){
+        if(!scanOn){
+            setScanOn(value => !value)
+            startQrCode()
+        }else{
+            scanner.stop().then(() => {
+                setScanOn(value => !value);
+            })
         }
+    }
 
 
-        return(
-            <>
-                <Flex flexDir="column" justifyContent="space-evenly" alignItems="center">
-                    <Flex flexDir="column" zIndex="0" justifyContent="center" alignItems="center" sx={{overflow: "hidden"}} position="relative" borderRadius="1000rem" border="solid 2px white" height={{base:"19em",md:"md"}} width={{base:"19em",md:"md"}}>
-                        <Box id="reader" zIndex="-1" display={scanOn? "inline":"none"} width={{base:"33em",md:"xl"}} height={{base:"33em",md:"xl"}}></Box>
-                        {scanOn?        
-                            <Flex position="absolute" border="white 3px solid" p="7em" top="2em">
-                                <Text>Scan Me</Text>
-                            </Flex>   
-                            :
-                            <Flex flexDir="column" justifyContent="center" alignItems="center">
-                                <UserProfile/>
-                                <Text>{name}</Text>
-                                <Heading position="relative">{entryType}</Heading>
-                            </Flex>
-                        }
-                    </Flex>
-                    <Button colorScheme="teal" width="70%" height="3em" borderRadius="2em" marginTop="2em" onClick={toggleScan}>{!scanOn? "Start Scan" : "Stop Scan"}</Button>
-
+    return(
+        <>
+            <NavBar/>
+            <Flex flexDir="column" justifyContent="space-evenly" alignItems="center">
+                <Flex flexDir="column" zIndex="0" justifyContent="center" alignItems="center" sx={{overflow: "hidden"}} position="relative" borderRadius="1000rem" border="solid 2px white" height={{base:"19em",md:"md"}} width={{base:"19em",md:"md"}}>
+                    <Box id="reader" zIndex="-1" display={scanOn? "inline":"none"} width={{base:"33em",md:"xl"}} height={{base:"33em",md:"xl"}}></Box>
+                    {scanOn?        
+                        <Flex position="absolute" border="white 3px solid" p="7em" top="2em">
+                            <Text>Scan Me</Text>
+                        </Flex>   
+                        :
+                        <Flex flexDir="column" justifyContent="center" alignItems="center">
+                            <UserProfile/>
+                            <Text>{name}</Text>
+                            <Heading position="relative">{entryType}</Heading>
+                        </Flex>
+                    }
+                </Flex>
+                <Button colorScheme="teal" width={{base:"60vw",sm:"50vw",md:"15em"}} height="3em" borderRadius="2em" marginTop="2em" onClick={toggleScan}>{!scanOn? "Start Scan" : "Stop Scan"}</Button>
+                <Flex fledDir="row" justifyContent="space-around" alignItems="center" backgroundColor="gray.200" width="90vw" height="6em" marginTop="4em" borderRadius="2rem">
+                <Stat padding="0.5em" width="0.5em" backgroundColor="purple.600"  boxShadow='xs' rounded='xl' bg='white' margin="0.5em" color="black">
+                        <StatLabel fontWeight="bold" fontSize="1em">Last Entry</StatLabel>
+                        <StatNumber>{entryObject? entryObject.entry_date_string : "Loading"}, {entryObject? entryObject.entry_time_string : "Loading"}</StatNumber>
+                    </Stat>
+                    <Stat padding="0.5em" width="0.5em" backgroundColor="purple.600"  boxShadow='xs' rounded='xl' bg='white' margin="0.5em" color="black">
+                        <StatLabel fontWeight="bold" fontSize="1em">Last Entry</StatLabel>
+                        <StatNumber>{entryObject? entryObject.entry_date_string : "Loading"}, {entryObject? entryObject.entry_time_string : "Loading"}</StatNumber>
+                    </Stat> 
                 </Flex>
 
-                <Modal isOpen={isOpen} onClose={onClose}>
-                    <ModalOverlay />
-                    <ModalContent backgroundColor="green">
-                        <ModalBody>
-                            <Flex height="10em" justifyContent="space-between" alignItems="center" flexDir="column">
-                                <Text fontWeight="medium">You have been:</Text>
-                                <Text fontWeight="bold" fontSize="4xl">{entryType}</Text>
-                                <Button onClick={onClose} marginLeft="auto" marginRight="auto">Complete</Button>
-                            </Flex>
-                        </ModalBody>
-                    </ModalContent>
-                </Modal>
-            </>
+            </Flex>
 
-        )   
-    }
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent backgroundColor="green">
+                    <ModalBody>
+                        <Flex height="10em" justifyContent="space-between" alignItems="center" flexDir="column">
+                            <Text fontWeight="medium">You have been:</Text>
+                            <Text fontWeight="bold" fontSize="4xl">{entryType}</Text>
+                            <Button onClick={onClose} marginLeft="auto" marginRight="auto">Complete</Button>
+                        </Flex>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
+
+    )   
+}
 
--- entry_system_website/pages/index.js
+++ entry_system_website/pages/index.js
@@ -7,7 +7,8 @@
   import { LockIcon, PlusSquareIcon, SunIcon , UnlockIcon} from '@chakra-ui/icons';
   import UserProfile from '../components/React/Profile';
   import {FaUserCircle} from "react-icons/fa";
-  import { HamburgerIcon } from '@chakra-ui/icons'
+  import { HamburgerIcon } from '@chakra-ui/icons';
+import NavBar from '../components/React/View/NavBar'
 
   const initialRoleLandingPages = {
     "teacher": "RowView",
@@ -35,71 +36,7 @@
 
     return (
       <Flex flexDir="column" alignItems="center">
-        <Flex flexDir="row" justifyContent="space-between" alignItems="center" padding="1em" width="100vw" marginBottom="2em">
-          <HStack>
-            <svg
-              width="35"
-              height="35"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M18 10.5C19.6569 10.5 21 11.8431 21 13.5V19.5C21 21.1569 19.6569 22.5 18 22.5H6C4.34315 22.5 3 21.1569 3 19.5V13.5C3 11.8431 4.34315 10.5 6 10.5V7.5C6 4.18629 8.68629 1.5 12 1.5C15.3137 1.5 18 4.18629 18 7.5V10.5ZM12 3.5C14.2091 3.5 16 5.29086 16 7.5V10.5H8V7.5C8 5.29086 9.79086 3.5 12 3.5ZM18 12.5H6C5.44772 12.5 5 12.9477 5 13.5V19.5C5 20.0523 5.44772 20.5 6 20.5H18C18.5523 20.5 19 20.0523 19 19.5V13.5C19 12.9477 18.5523 12.5 18 12.5Z"
-                fill="currentColor"
-              />
-            </svg>
-            <Heading fontSize="1.3em">Securas</Heading>
-
-          </HStack>
-
-
-          <HStack>
-            <HStack display={{base:"none",xl:"inline"}}>
-              {user ? 
-                <>
-                  <Button leftIcon={<UnlockIcon />} onClick={Firebase_signOut} colorScheme='teal' variant='ghost'>
-                    Sign Out
-                  </Button>
-                  <UserProfile/>
-                </>
-                :
-                <>
-                  <Button as="a" href="/SignInPage" leftIcon={<LockIcon />} colorScheme='teal' variant='ghost'>
-                    Sign in 
-                  </Button>
-                  <Button as="a" href="/SignUpPage" leftIcon={<LockIcon />} colorScheme='teal' variant='ghost'>
-                    Sign Up 
-                  </Button>
-                </>
-              }
-
-            </HStack>
-            <Menu>
-              <MenuButton display={{base:"inline",xl:"none"}} as={IconButton} variant="unstyled" icon={<HamburgerIcon w="2em" h="2em"/>}>Menu
-              </MenuButton>
-              <MenuList>
-                {user ? 
-                  <>
-                    <MenuItem as="a" href="/account"icon={<FaUserCircle/>}>Account</MenuItem>
-                    <MenuItem  onClick={Firebase_signOut} icon={<UnlockIcon />}>Sign Out</MenuItem>
-                  </>
-                  :
-                  <>
-                    <MenuItem as="a" href="/SignInPage">Sign In</MenuItem>
-                    <MenuItem as="a" href="/SignUpPage">Create Account</MenuItem>
-                  </>
-                }
-              </MenuList>
-            </Menu>
-            <Button onClick={toggleColorMode}>
-                <SunIcon/>
-            </Button>
-          </HStack>
-
-
-        </Flex>
+        <NavBar/>
 
         {user?
             <Flex bgGradient='linear(to-b, blue.400, green.500 )' as="a" _hover={{bgGradient: 'linear(to-b, green.100, green.500 )'}} href={dashboardSelector} justifyContent="center" boxShadow="xl"alignItems="center" borderRadius="1000rem" width={{base:"80vw",sm:"60vw", md:"md"}} height={{base:"80vw",sm:"60vw",md:"md"}}>
