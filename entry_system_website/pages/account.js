c4b5f860b3dd75f2ecd24e621b961fefbb5344f4
grid-rider <62141711+grid-rider@users.noreply.github.com>
2022-03-22 12:40:53 +0100

    implemented account icon change and settings

--- entry_system_website/pages/account.js
+++ entry_system_website/pages/account.js
@@ -1,6 +1,6 @@
 import { useAuth } from '../components/Firebase/Context/authUserContext';
 import UserProfile from '../components/React/Profile';
-import { Box, Button, Flex, FormControl, FormErrorMessage, FormLabel, Heading, HStack, Input, InputGroup, InputRightElement, Modal, Select, useColorModeValue } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, FormControl, FormErrorMessage, FormLabel, Heading, HStack, Input, InputGroup, InputRightElement, Modal, Select, Text, useColorModeValue } from '@chakra-ui/react';
 import {
     SunIcon,
     ViewIcon,
@@ -9,64 +9,92 @@ import {
 import { useColorMode } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
+import NavBar from '../components/React/View/NavBar';
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+
 
 export default function Account(){
 
     let { user } = useAuth();
+    let [invalidSignUp, setInvalidSignUp] = useState(false);
     let [editMode, setEditMode] = useState(false);
-    
-    if(!user){
-        return(<Text>Acess Denied: Please Log In</Text>)
-    }
+    let [iconURL, setIconURL] = useState("");
+    let [firstName, setFirstName] = useState("Loading.....");
+    let [lastName, setLastName] = useState("Loading.....");
+
+    //object schema for image file upload validation
+    const schema = yup.object().shape({
+        picture: yup
+            .mixed()
+            .test('required', "You need to provide a file", (value) =>{
+                return value && value.length
+            } )
+            .test("fileSize", "Image File To Larg", (value, context) => {
+                return value && value[0] && value[0].size <= 200000;
+            })
+    });
 
     //form handler
-    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
+    const { register, handleSubmit, formState: { errors, isSubmitting } } =  useForm({
+        resolver: yupResolver(schema),
+      });
+      
 
     function saveEditButtonHandler(data){
+        console.log(data)
+    }
 
+    function handleUserIconChange(event){
+        console.log(event.target.files[0])
+        console.log(URL.createObjectURL(event.target.files[0]))
+        setIconURL(URL.createObjectURL(event.target.files[0]))
     }
     
     return(
-        <Flex justifyContent="center" alignItems="center" height="100vh" flexDirection="column">
+        <>
+            <NavBar/>
+            <Flex justifyContent="center" alignItems="center"  flexDirection="column">
+                <Flex flexDirection="column" alignItems="center" borderRadius="12" width="80vw" background="gray.100" p="12" justifyContent="space-evenly" color="black">
+                    <Heading mb="1em">Edit Account</Heading>
+                    <Avatar width="8em" height="8em" src={iconURL}/>
+                    <Box width="100%">
+                        <form onSubmit={handleSubmit(saveEditButtonHandler)}>
+                            <FormControl  m={1} isInvalid={errors.picture} onChange={handleUserIconChange}>
+                                <FormLabel>User Icon</FormLabel>
+                                <Input p="0.1em" variant="unstyled"  {...register("picture")} type="file" name="picture"/>
+                                <FormErrorMessage>
+                                {errors.picture && errors.picture.message}
+                                </FormErrorMessage>
+                            </FormControl>
+                            <FormControl  m={1} isInvalid={errors.firstName}>
+                                <FormLabel>First Name</FormLabel>
+                                <Input placeholder='Enter First Name' {...register("firstName", { required: {value: true ,message: "Entry Required"}})}/>
+                                <FormErrorMessage>
+                                        {errors.firstName && errors.firstName.message}
+                                </FormErrorMessage>
+                            </FormControl>
 
-            <Flex flexDirection="column" borderRadius="12" background={formBackground} p="12" justifyContent="space-evenly">
-                <Heading mb="1em">Sign Up Form</Heading>
-                
+                            <FormControl  m={1} isInvalid={errors.lastName}>
+                                <FormLabel>Last Name</FormLabel>
+                                <Input placeholder='Enter Last Name' {...register("lastName", { required: {value: true ,message: "Entry Required"}})}/>
+                                <FormErrorMessage>
+                                        {errors.lastName && errors.lastName.message}
+                                </FormErrorMessage>
+                            </FormControl>
 
-                <form onSubmit={handleSubmit(saveEditButtonHandler)}>
-                    <FormControl  m={1} isInvalid={errors.email}>
-                        <FormLabel>Email</FormLabel>
-                        <Input type="email" placeholder='Enter Email' {...register("email", { required: {value: true ,message: "Entry Required"}})}/>
-                        <FormErrorMessage>
-                            {errors.email && errors.email.message}
-                        </FormErrorMessage>
-                    </FormControl>
+                            <HStack mt="2em">
+                                <Button width="100%" colorScheme="teal" type='submit' isLoading={isSubmitting}>Set Account</Button>
+                            </HStack>
+                        </form>
+                    </Box>
 
-                    <FormControl  m={1} isInvalid={errors.firstName}>
-                        <FormLabel>First Name</FormLabel>
-                        <Input placeholder='Enter First Name' {...register("firstName", { required: {value: true ,message: "Entry Required"}})}/>
-                        <FormErrorMessage>
-                                {errors.firstName && errors.firstName.message}
-                        </FormErrorMessage>
-                    </FormControl>
 
-                    <FormControl  m={1} isInvalid={errors.lastName}>
-                        <FormLabel>Last Name</FormLabel>
-                        <Input placeholder='Enter Last Name' {...register("lastName", { required: {value: true ,message: "Entry Required"}})}/>
-                        <FormErrorMessage>
-                                {errors.lastName && errors.lastName.message}
-                        </FormErrorMessage>
-                    </FormControl>
+                    <div style={{display: !invalidSignUp ? "none" : "block", color: "red", fontSize: "1em", margin: "0 auto"}}>Invalid Email or Password</div>
+                </Flex>
+            </Flex>       
+        </>
 
-                    <HStack mt="2em">
-                        <Button width="100%" colorScheme="teal" type='submit' isLoading={isSubmitting}>Sign Up</Button>
-                        <Button onClick={toggleColorMode}><SunIcon/></Button>
-                    </HStack>
-                </form>
-
-                <div style={{display: !invalidSignUp ? "none" : "block", color: "red", fontSize: "1em", margin: "0 auto"}}>Invalid Email or Password</div>
-            </Flex>
-        </Flex>
 
     )
 }
